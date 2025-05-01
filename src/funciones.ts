9d9966a270ad12ded7efd9523b064130e63b5b88
Javier Yanes <alu0101391960@ull.edu.es>
2025-05-01 16:47:57 +0000

    Pruebas vitest añadidas

--- src/funciones.ts
+++ src/funciones.ts
@@ -7,3 +7,18 @@
 export function suma(a: number, b: number): number {
     return a + b;
   }
+
+/**
+ * Funcion que resta el numero b al a
+ * @param a - primer numero
+ * @param b - segundo numero
+ * @returns Devuelve a - b
+ * ```typescript
+ * resta(1,7) = -6
+ * resta(15,3) = 12
+ * ```
+ */
+export function resta(a: number, b: number): number {
+  return a - b;
+}
+  
